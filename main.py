d367eae43752e6164b85efb72fe48b3d9b80a9c0
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-27 21:38:57 -0700

    Lab4-5-1 Camerino A

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    numbers=[]
+    total=0
+    value=0
+    while value<5:
+        randomnum = random.randint(0,100)
+        numbers.append(randomnum)
+        total += randomnum 
+        value += 1    
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
